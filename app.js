35063453444384f5debf6957d32f6dc04da822da
Yuri Maciel <yuri.maciel@southsystem.com.br>
2022-11-15 16:02:52 -0300

    body-parser

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5050
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(cors({
   origin: '*'
 }));
